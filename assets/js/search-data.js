54f098739aa63644529592f675212c06a66c5dbb
Kevin DeMarco <kevin.demarco@gmail.com>
2021-10-01 01:33:38 -0400

    Deploy updates

---
+++ assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Launch!",
+        "excerpt":"goto; launched; ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "https://goto-semi-colon.com/jekyll/update/2021/09/30/launch.html"
+      }]
